a13e5f2b453b9df6d112c72979ac76b4c8dfb1c0
krissy <krabino@globalpersonals.co.uk>
2015-09-24 16:28:11 +0100

    Update README and some stylez.

--- app/timesheet.jsx
+++ app/timesheet.jsx
@@ -3,13 +3,16 @@ var React = require("react");
 module.exports = React.createClass({displayName: 'Timesheet',
   render: function() {
     return (
-      <ul id="timesheet">
-      {
-        this.props.timeSheet.map(function(timeEntry) {
-          return <li>Task: {timeEntry[0]}, Hours: {timeEntry[1]}</li>
-        })
-      }
-      </ul>
+      <div id="timesheet">
+        <h3>Logged today:</h3>
+        <ul>
+        {
+          this.props.timeSheet.map(function(timeEntry) {
+            return <li><p><strong>{timeEntry[0]}:</strong> {timeEntry[1]}</p></li>
+          })
+        }
+        </ul>
+      </div>
     );
   }
 });
